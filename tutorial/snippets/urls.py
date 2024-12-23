18b7d51f3ff6035c5444d633c4233fb8d1a22d57
Vitalii Derda <derdavitaliy@gmail.com>
2024-12-23 17:57:22 +0200

    feat: complete part 1: serialization

---
+++ tutorial/snippets/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.snippet_list, name='snippet-list'),
+    path('<int:pk>/', views.snippet_detail, name='snippet-detail'),
+]
