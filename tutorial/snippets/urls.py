ea6ee519ce78785467fe9a72fe885e0d4d7cc9a6
Vitalii Derda <derdavitaliy@gmail.com>
2024-12-23 18:48:35 +0200

    feat: complete part 2: request and response

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
@@ -6,3 +7,5 @@ urlpatterns = [
     path('', views.snippet_list, name='snippet-list'),
     path('<int:pk>/', views.snippet_detail, name='snippet-detail'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
